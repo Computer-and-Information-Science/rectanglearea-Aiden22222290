011ec41dbf230da79bb7c6cdeea57383af911445
Aiden22222290 <amalt0826@gmail.com>
2025-09-04 21:14:29 +0000

    Area

--- main.cpp
+++ main.cpp
@@ -5,5 +5,14 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double area;
+  area = length*width;
+  double perimiter;
+  perimiter = (length*2) + (width*2);
 
+  cout << "Rectangle Properties: " << endl;
+  cout << "Length: " << length <<endl;
+  cout << "Width: " << width <<endl;  
+  cout << "Area: " << area <<endl;
+  cout << "Perimiter: " << perimiter <<endl;
 }
